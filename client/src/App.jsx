ef08debe3fccc145a2039dcae1275f0989fefc10
roshray <roshanroy759@gmail.com>
2022-03-12 00:23:17 +0530

    components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-
+import { Navbar, Welcome, Footer } from './components'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Footer />
     </div>
   )
 }
