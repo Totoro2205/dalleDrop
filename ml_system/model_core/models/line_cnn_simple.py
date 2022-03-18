675c16a37c811abd379d2c6b37a8238c0fca850d
roshray <roshanroy759@gmail.com>
2022-03-18 23:07:06 +0530

    model

--- ml_system/model_core/models/line_cnn_simple.py
+++ ml_system/model_core/models/line_cnn_simple.py
@@ -1,3 +1,18 @@
 # a model that can read multiple images in a character!
 
- 
+ import math
+from typing import Any, Dict
+ import argparseimport math
+ import torch
+ import torch.nn as nn
+
+ from .cnn import CBB, IMAGE_SIZE
+
+ WINDOW_WIDTH = 28
+ WINDOW_STRIDE = 28
+
+ class LineCNNSimple(nn.module):
+     def __init__(
+         self,
+         data_config
+     )
