1ab04d6ba717d5d4447cb606621ba13187911383
roshray <roshanroy759@gmail.com>
2022-04-11 15:26:52 +0530

    frontend/dalle

--- frontend/pages/nft/[id].tsx
+++ frontend/pages/nft/[id].tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import { GetServerSideProps } from 'next';
+import { sanityClient, urlFor } from '../../sanity'
+import { Collection } from '../../typing';
 
-function NFTDropPage() {
+
+interface Props {
+    collection: Collection
+}
+function NFTDropPage({collection}: Props) {
     // Auth
     const connectWithMetamask = useMetamask()
     const address = useAddress()
@@ -9,33 +16,33 @@ function NFTDropPage() {
     // ---
   return (
     <div className='flex h-screen flex-col lg:grid lg:grid-cols-10 '>
-        <div className='bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4'>
+        <div className='bg-gradient-to-br from-cyan-800 to-zinc-900 lg:col-span-4'>
             <div className='flex flex-col items-center justify-center py-2 lg:min-h-screen '>
                 <div className="rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2">
                     <img 
                         className='w-44 rounded-xl object-cover lg:h-96 lg:w-72' 
-                        src="https://links.papareact.com/8sg" 
+                        src={urlFor(collection.previewImage).url()}
                         alt="" 
                     />
                 </div>
                 <div className='text-center p-5 space-y-2'>
-                    <h1 className="text-4 font-bold text-white">NFT Mining </h1>
-                    <h2 className='text-xl text-gray-300'> A Full Stack NFT Mining Platform</h2>
+                    <h1 className="text-4 font-bold text-white">{collection.nftCollectionName}</h1>
+                    <h2 className='text-xl text-gray-300'> {collection.description}</h2>
                 </div>
             </div>
         </div>
         {/* Right */}
-        <div className='flex flex-1 flex-col p-12 lg:col-span-6'>
+        <div className='flex flex-1 flex-col bg-gradient-to-br from-green-900 to-zinc-900 p-12 lg:col-span-6'>
             {/* Header */}
             <header className='flex items-center justify-between'>
                 <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'> The{' '}
-                    <span className='font-extrabold underline decoration-pink-600/50'>Web3
+                    <span className='font-extrabold underline decoration-slate-100/50'>Web3
                     </span>{' '} 
                 NFT Market Place
                 </h1>
                 <button
                     onClick={() => connectWithMetamask()} 
-                    className='rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base'
+                    className='rounded-full bg-violet-800 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base'
                 >
                     {address ? 'Sign Out' : 'Sign In'}
                 </button>
@@ -45,17 +52,17 @@ function NFTDropPage() {
             {/* Content*/}
             <div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0'>
                 <img 
-                    className='w-80 objext-cover pb-10 lg:h-40' src="https://links.papareact.com/bdy" 
+                    className='w-80 rounded-xl bg-gradient-to-br from-slate-400 to-violet-600 p-1 lg:h-40' src={urlFor(collection.mainImage).url()}
                     alt="" 
                 />
-                <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold"> Full Stack Club | The Drop
+                <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold"> {collection.title}
                 </h1>
 
                 <p className="pt-2 text-xl text-green-500"> 13 / 21 NFT's</p>
             </div>
 
             {/* Mint button*/}
-            <button className='h-16 w-full rounded-full bg-red-600 text-white font-bold'>
+            <button className='h-16 w-full rounded-full bg-violet-800 text-white font-bold'>
                 Mint NFT (0.01 ETH)
             </button>
         </div>
@@ -65,3 +72,47 @@ function NFTDropPage() {
 }
 
 export default NFTDropPage
+
+export const getServerSideProps: GetServerSideProps = async ({params}) => {
+    const query = `*[_type == 'collection' && slug.current == $id][0]{
+        _id,
+        title,
+        address,
+        description,
+        nftCollectionName,
+        mainImage {
+          asset
+        },
+        previewImage {
+          asset
+        },
+        slug {
+          current
+        },
+        creator-> {
+          _id,
+          name,
+          address,
+          slug {
+            current
+         },
+        },
+    }`
+
+    const collection = await sanityClient.fetch(query, {
+        id: params?.id
+    })
+
+    if (!collection) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
+            collection
+        }
+    }
+}
+
